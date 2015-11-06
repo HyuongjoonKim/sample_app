248185a4eda643aec1b54c83848adec4ecaf7c72
Hyein Kim <h.kim@japantaxi.co.jp>
2015-11-06 10:40:34 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "StaticPages" do
     visit '/static_pages/home'
     expect(page).to have_content('Sample App')
     end
+
+    it "should have ths right title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
@@ -13,12 +18,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('help')
     end
+
+    it "should have ths right title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "Abount page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have ths right title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
